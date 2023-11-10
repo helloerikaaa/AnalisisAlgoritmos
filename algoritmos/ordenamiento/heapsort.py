52a2592ab51ff7d1669df42583b00831d96374c6
Erika Sánchez Femat <erikasafe@gmail.com>
2023-11-10 15:18:04 -0600

    [REFACTOR] Heapsort
    [Author] Erika Sánchez-Femat
    
    -- Refactor del código de heapsort

--- algoritmos/ordenamiento/heapsort.py
+++ algoritmos/ordenamiento/heapsort.py
@@ -4,33 +4,28 @@ import matplotlib.pyplot as plt
 
 
 def heapify(datos, n, i):
-    mas_grande = i 
+    mas_grande = i
     izq = 2 * i + 1
     der = 2 * i + 2
 
-    
     if izq < n and datos[izq] > datos[mas_grande]:
         mas_grande = izq
 
-    
     if der < n and datos[der] > datos[mas_grande]:
         mas_grande = der
 
-    
     if mas_grande != i:
         datos[i], datos[mas_grande] = datos[mas_grande], datos[i]
 
-        
         heapify(datos, n, mas_grande)
+
 
 def heapsort(datos):
     n = len(datos)
 
-    
     for i in range(n // 2 - 1, -1, -1):
         heapify(datos, n, i)
 
-    
     for i in range(n - 1, 0, -1):
         datos[i], datos[0] = datos[0], datos[i]
         heapify(datos, i, 0)
